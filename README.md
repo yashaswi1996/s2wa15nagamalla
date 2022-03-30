6dde35e1db4856cc9863f9a8768da27e13ea44ea
yashaswi1996 <46136157+yashaswi1996@users.noreply.github.com>
2022-03-30 15:25:44 -0500

    comit dome

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # s2wa15nagamalla
 Express generator - Assignment 9
+[Server Hosted](https://s2wa15nagamalla.herokuapp.com/)
--- routes/mydata.js
+++ routes/mydata.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var times = 0;
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  times++;
-  res.send(`User accesses are:${times}`);
+  res.render('mydata', { title: 'Yashaswi Nagamalla' });
 });
 
 module.exports = router;
